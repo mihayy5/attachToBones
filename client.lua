3abc0fcd1c40d08876409201951fbc7324fe706c
Mihaita Grosu <mihayy56@gmail.com>
2021-01-22 09:00:43 +0100

    Update v1.4
    -Added all bone_attach shared functions;
    -Added all functions to client side;
    -Even more optimization;

--- client.lua
+++ client.lua
@@ -2,12 +2,15 @@ local AttachementsTable = {};
 
 local m_sin = math.sin;
 local m_cos = math.cos;
+local elm = isElement;
+local tonr = tonumber;
 local _getElementBoneMatrix = getElementBoneMatrix;
 local _setElementMatrix = setElementMatrix;
 local _isElementOnScreen = isElementOnScreen;
 local _setElementPosition = setElementPosition;
-local sroll, croll, spitch, cpitch, syaw, cyaw, boneMat, rotMat, finalMatrix = false, false, false, false, false, false, {}, {}, {};
-local v7, v8, v9 = false, false, false;
+local boneMat, rotMat, finalMatrix = {}, {}, {};
+local notOnScrenElements = {};
+
 local boneMatOne = false;
 local boneMatTwo = false;
 local boneMatTree = false;
@@ -40,22 +43,33 @@ local FOR = false;
 local FIVE = false;
 local SIX = false;
 
-local notOnScrenElements = {};
-
-
 addEvent("sync_attachements", true);
 addEvent("sync_detachements", true);
 addEvent("sync_newcomeattachements", true);
+addEvent("sync_pos_attachements", true);
 
-addEventHandler("sync_attachements", root, function(data)
-	if data then
-		AttachementsTable[#AttachementsTable + 1] = data;
+local function calculateMatrix(orx, ory, orz)
+	if tonr(orx) and tonr(ory) and tonr(orz) then
+		local sroll, croll, spitch, cpitch, syaw, cyaw = m_sin(m_rad(orz)), m_cos(m_rad(orz)), m_sin(m_rad(ory)), m_cos(m_rad(ory)), m_sin(m_rad(orx)), m_cos(m_rad(orx));
+		local rotMat = {
+			{sroll * spitch * syaw + croll * cyaw, sroll * cpitch, sroll * spitch * cyaw - croll * syaw},
+			{croll * spitch * syaw - sroll * cyaw, croll * cpitch, croll * spitch * cyaw + sroll * syaw},
+			{cpitch * syaw, -spitch, cpitch * cyaw}
+		};
+		return rotMat;
+	end
+	return false;
+end
+
+addEventHandler("sync_attachements", root, function(element, data)
+	if elm(element) and data then
+		AttachementsTable[element] = data;
 	end
 end);
 
-addEventHandler("sync_detachements", root, function(position)
-	if tonumber(position) then
-		table.remove(AttachementsTable, position);
+addEventHandler("sync_detachements", root, function(elm)
+	if AttachementsTable[elm] then
+		AttachementsTable[elm] = nil;
 	end
 end);
 
@@ -64,80 +78,158 @@ addEventHandler("onClientResourceStart", resourceRoot, function()
 end);
 
 addEventHandler("sync_newcomeattachements", resourceRoot, function(theTable)
-	AttachementsTable = {};
 	if type(theTable) == "table" then
+		AttachementsTable = {};
 		AttachementsTable = theTable;
 	end
 end);
 
+addEventHandler("sync_pos_attachements", resourceRoot, function(element, theTable)
+	if type(theTable) == "table" then
+		AttachementsTable[element] = theTable;
+	end
+end);
+
 addEventHandler("onClientPedsProcessed", root, function()
-	for i = 1, #AttachementsTable do
-		local v = AttachementsTable[i];
-		if v then
-			local elm = v[1];
-			local ped = v[2];
-			if _isElementOnScreen(ped) then
-				notOnScrenElements[elm] = false;
-				boneMat = _getElementBoneMatrix(ped, v[3]);
-				v7, v8, v9 = v[7], v[8], v[9];
-				sroll, croll, spitch, cpitch, syaw, cyaw = m_sin(v9), m_cos(v9), m_sin(v8), m_cos(v8), m_sin(v7), m_cos(v7);
-				rotMat = {
-					{sroll * spitch * syaw + croll * cyaw, sroll * cpitch, sroll * spitch * cyaw - croll * syaw},
-					{croll * spitch * syaw - sroll * cyaw, croll * cpitch, croll * spitch * cyaw + sroll * syaw},
-					{cpitch * syaw, -spitch, cpitch * cyaw}
-				};
-				boneMatOne = boneMat[1];
-				boneMatTwo = boneMat[2];
-				boneMatTree = boneMat[3];
-				boneMatFor = boneMat[4];
-				boneMatOneOne = boneMatOne[1];
-				boneMatOneTwo = boneMatOne[2];
-				boneMatOneTree = boneMatOne[3];
-				boneMatTwoOne = boneMatTwo[1];
-				boneMatTwoTwo = boneMatTwo[2];
-				boneMatTwoTree = boneMatTwo[3];
-				boneMatTreeOne = boneMatTree[1];
-				boneMatTreeTwo = boneMatTree[2];
-				boneMatTreeTree = boneMatTree[3];
-				boneMatForOne = boneMatFor[1];
-				boneMatForTwo = boneMatFor[2];
-				boneMatForTree = boneMatFor[3];
-				rotMatOne = rotMat[1];
-				rotMatTwo = rotMat[2];
-				rotMatTree = rotMat[3];
-				rotMatOneOne = rotMatOne[1];
-				rotMatOneTwo = rotMatOne[2];
-				rotMatOneTree = rotMatOne[3];
-				rotMatTwoOne = rotMatTwo[1];
-				rotMatTwoTwo = rotMatTwo[2];
-				rotMatTwoTree = rotMatTwo[3];
-				rotMatTreeOne = rotMatTree[1];
-				rotMatTreeTwo = rotMatTree[2];
-				rotMatTreeTree = rotMatTree[3];
-				FOR = v[4];
-				FIVE = v[5];
-				SIX = v[6];
-				finalMatrix = {
-					{boneMatTwoOne * rotMatOneTwo + boneMatOneOne * rotMatOneOne + rotMatOneTree * boneMatTreeOne,
-					boneMatTreeTwo * rotMatOneTree + boneMatOneTwo * rotMatOneOne + boneMatTwoTwo * rotMatOneTwo,
-					boneMatTwoTree * rotMatOneTwo + boneMatTreeTree * rotMatOneTree + rotMatOneOne * boneMatOneTree, 0},
-					{rotMatTwoTree * boneMatTreeOne + boneMatTwoOne * rotMatTwoTwo + rotMatTwoOne * boneMatOneOne,
-					boneMatTreeTwo * rotMatTwoTree + boneMatTwoTwo * rotMatTwoTwo + boneMatOneTwo * rotMatTwoOne,
-					rotMatTwoOne * boneMatOneTree + boneMatTreeTree * rotMatTwoTree + boneMatTwoTree * rotMatTwoTwo, 0},
-					{boneMatTwoOne * rotMatTreeTwo + rotMatTreeTree * boneMatTreeOne + rotMatTreeOne * boneMatOneOne,
-					boneMatTreeTwo * rotMatTreeTree + boneMatTwoTwo * rotMatTreeTwo + rotMatTreeOne * boneMatOneTwo,
-					rotMatTreeOne * boneMatOneTree + boneMatTreeTree * rotMatTreeTree + boneMatTwoTree * rotMatTreeTwo, 0},
-					{FOR * boneMatOneOne + FIVE * boneMatTwoOne + SIX * boneMatTreeOne + boneMatForOne,
-					FOR * boneMatOneTwo + FIVE * boneMatTwoTwo + SIX * boneMatTreeTwo + boneMatForTwo,
-					FOR * boneMatOneTree + FIVE * boneMatTwoTree + SIX * boneMatTreeTree + boneMatForTree, 1}
-				};
-				_setElementMatrix(elm, finalMatrix);
-			else
-				if not notOnScrenElements[elm] then
-					_setElementPosition(elm, 0, 0, -10000);
-					notOnScrenElements[elm] = true;
-				end
+	for element,data in next, AttachementsTable do
+		local ped = data[1];
+		if _isElementOnScreen(ped) then
+			notOnScrenElements[element] = false;
+			boneMat = _getElementBoneMatrix(ped, data[2]);
+			rotMat = data[6];
+			boneMatOne = boneMat[1];
+			boneMatTwo = boneMat[2];
+			boneMatTree = boneMat[3];
+			boneMatFor = boneMat[4];
+			boneMatOneOne = boneMatOne[1];
+			boneMatOneTwo = boneMatOne[2];
+			boneMatOneTree = boneMatOne[3];
+			boneMatTwoOne = boneMatTwo[1];
+			boneMatTwoTwo = boneMatTwo[2];
+			boneMatTwoTree = boneMatTwo[3];
+			boneMatTreeOne = boneMatTree[1];
+			boneMatTreeTwo = boneMatTree[2];
+			boneMatTreeTree = boneMatTree[3];
+			boneMatForOne = boneMatFor[1];
+			boneMatForTwo = boneMatFor[2];
+			boneMatForTree = boneMatFor[3];
+			rotMatOne = rotMat[1];
+			rotMatTwo = rotMat[2];
+			rotMatTree = rotMat[3];
+			rotMatOneOne = rotMatOne[1];
+			rotMatOneTwo = rotMatOne[2];
+			rotMatOneTree = rotMatOne[3];
+			rotMatTwoOne = rotMatTwo[1];
+			rotMatTwoTwo = rotMatTwo[2];
+			rotMatTwoTree = rotMatTwo[3];
+			rotMatTreeOne = rotMatTree[1];
+			rotMatTreeTwo = rotMatTree[2];
+			rotMatTreeTree = rotMatTree[3];
+			TREE = data[3];
+			FOR = data[4];
+			FIVE = data[5];
+			finalMatrix = {
+				{boneMatTwoOne * rotMatOneTwo + boneMatOneOne * rotMatOneOne + rotMatOneTree * boneMatTreeOne,
+				boneMatTreeTwo * rotMatOneTree + boneMatOneTwo * rotMatOneOne + boneMatTwoTwo * rotMatOneTwo,
+				boneMatTwoTree * rotMatOneTwo + boneMatTreeTree * rotMatOneTree + rotMatOneOne * boneMatOneTree, 0},
+				{rotMatTwoTree * boneMatTreeOne + boneMatTwoOne * rotMatTwoTwo + rotMatTwoOne * boneMatOneOne,
+				boneMatTreeTwo * rotMatTwoTree + boneMatTwoTwo * rotMatTwoTwo + boneMatOneTwo * rotMatTwoOne,
+				rotMatTwoOne * boneMatOneTree + boneMatTreeTree * rotMatTwoTree + boneMatTwoTree * rotMatTwoTwo, 0},
+				{boneMatTwoOne * rotMatTreeTwo + rotMatTreeTree * boneMatTreeOne + rotMatTreeOne * boneMatOneOne,
+				boneMatTreeTwo * rotMatTreeTree + boneMatTwoTwo * rotMatTreeTwo + rotMatTreeOne * boneMatOneTwo,
+				rotMatTreeOne * boneMatOneTree + boneMatTreeTree * rotMatTreeTree + boneMatTwoTree * rotMatTreeTwo, 0},
+				{TREE * boneMatOneOne + FOR * boneMatTwoOne + FIVE * boneMatTreeOne + boneMatForOne,
+				TREE * boneMatOneTwo + FOR * boneMatTwoTwo + FIVE * boneMatTreeTwo + boneMatForTwo,
+				TREE * boneMatOneTree + FOR * boneMatTwoTree + FIVE * boneMatTreeTree + boneMatForTree, 1}
+			};
+			_setElementMatrix(element, finalMatrix);
+		else
+			if not notOnScrenElements[element] then
+				_setElementPosition(element, 0, 0, -10000);
+				notOnScrenElements[element] = true;
 			end
 		end
 	end
 end);
+
+addEventHandler("onClientElementDestroy", root, function()
+	if AttachementsTable[source] then
+		AttachementsTable[source] = nil;
+	end
+end);
+
+function attachElementToBone(element, ped, bone, offx, offy, offz, offrx, offry, offrz)
+	if elm(element) and elm(ped) and tonr(bone) then
+		if isElementAttachedToBone(element) then return false; end
+		local offrx = offrx or 0;
+		local offry = offry or 0;
+		local offrz = offrz or 0;
+		local rotMat = calculateMatrix(offrx, offry, offrz);
+		local table = {ped, bone, tonr(offx) or 0, tonr(offy) or 0, tonr(offz) or 0, rotMat};
+		setElementCollisionsEnabled(element, false);
+		AttachementsTable[element] = table;
+		return true;
+	end
+	return false;
+end
+
+function detachElementFromBone(element)
+	if elm(element) then
+		if AttachementsTable[element] then
+			AttachementsTable[element] = nil;
+			return true;
+		end
+	end
+	return false;
+end
+
+function isElementAttachedToBone(element)
+	if elm(element) then
+		if AttachementsTable[element] then
+			return true;
+		end
+	end
+	return false;
+end
+
+function getElementBoneAttachmentDetails(element)
+	if elm(element) then
+		if AttachementsTable[element] then
+			return AttachementsTable[element];
+		end
+	end
+	return false;
+end
+
+function setElementBonePositionOffset(element, offsetx, offsety, offsetz)
+	if elm(element) then
+		local elmT = AttachementsTable[element] or nil;
+		if elmT then
+			local ped = elmT[1];
+			local bone = elmT[2];
+			local rotM = elmT[6];
+			local tableSynch = {ped, bone, offsetx, offsety, offsetz, rotM};
+			AttachementsTable[element] = tableSynch;
+			return true;
+		end
+	end
+	return false;
+end
+
+function setElementBoneRotationOffset(element, offsetrx, offsetry, offsetrz)
+	if elm(element) then
+		local elmT = AttachementsTable[element] or nil;
+		if elmT then
+			local ped = elmT[1];
+			local bone = elmT[2];
+			local ox = elmT[3];
+			local oy = elmT[4];
+			local oz = elmT[5];
+			local rotm = calculateMatrix(offsetrx, offsetry, offsetrz);
+			local tableSynch = {ped, bone, ox, oy, oz, rotm};
+			AttachementsTable[element] = tableSynch;
+			return true;
+		end
+	end
+	return false;
+end
--- server.lua
+++ server.lua
@@ -3,39 +3,51 @@ local Attachemenets = {};
 local elm = isElement;
 local m_rad = math.rad;
 local tonr = tonumber;
+local m_sin = math.sin;
+local m_cos = math.cos;
 
 addEvent("sync_newcomeplayer", true);
 
+local function calculateMatrix(orx, ory, orz)
+	if tonr(orx) and tonr(ory) and tonr(orz) then
+		local sroll, croll, spitch, cpitch, syaw, cyaw = m_sin(m_rad(orz)), m_cos(m_rad(orz)), m_sin(m_rad(ory)), m_cos(m_rad(ory)), m_sin(m_rad(orx)), m_cos(m_rad(orx));
+		local rotMat = {
+			{sroll * spitch * syaw + croll * cyaw, sroll * cpitch, sroll * spitch * cyaw - croll * syaw},
+			{croll * spitch * syaw - sroll * cyaw, croll * cpitch, croll * spitch * cyaw + sroll * syaw},
+			{cpitch * syaw, -spitch, cpitch * cyaw}
+		};
+		return rotMat;
+	end
+	return false;
+end
+
 function attachElementToBone(element, ped, bone, offx, offy, offz, offrx, offry, offrz)
 	if elm(element) and elm(ped) and tonr(bone) then
-		local table = {element, ped, bone, tonr(offx) or 0, tonr(offy) or 0, tonr(offz) or 0, m_rad(offrx) or 0, m_rad(offry) or 0, m_rad(offrz) or 0};
+		if isElementAttachedToBone(element) then return false; end
+		local offrx = offrx or 0;
+		local offry = offry or 0;
+		local offrz = offrz or 0;
+		local rotMat = calculateMatrix(offrx, offry, offrz);
+		local table = {ped, bone, tonr(offx) or 0, tonr(offy) or 0, tonr(offz) or 0, rotMat};
 		setElementCollisionsEnabled(element, false);
-		Attachemenets[#Attachemenets + 1] = table;
-		triggerClientEvent(root, "sync_attachements", resourceRoot, table);
+		Attachemenets[element] = table;
+		triggerClientEvent(root, "sync_attachements", resourceRoot, element, table);
 	end
 end
 
 function detachElementFromBone(element)
 	if elm(element) then
-		for i = 1, #Attachemenets do
-			local v = Attachemenets[i];
-			if v[1] == element then
-				table.remove(Attachemenets, i);
-				triggerClientEvent(root, "sync_detachements", resourceRoot, i);
-				break;
-			end
+		if Attachemenets[element] then
+			Attachemenets[element] = nil;
+			triggerClientEvent(root, "sync_detachements", resourceRoot, element);
 		end
 	end
 end
 
 addEventHandler("onElementDestroy", root, function()
-	for i = 1, #Attachemenets do
-		local v = Attachemenets[i];
-		if v[1] == source or v[2] == source then
-			table.remove(Attachemenets, i);
-			triggerClientEvent(root, "sync_detachements", resourceRoot, i);
-			break;
-		end
+	if Attachemenets[source] then
+		Attachemenets[source] = nil;
+		triggerClientEvent(root, "sync_detachements", resourceRoot, source);
 	end
 end);
 
@@ -44,3 +56,56 @@ addEventHandler("sync_newcomeplayer", root, function()
 		triggerClientEvent(client, "sync_newcomeattachements", resourceRoot, Attachemenets);
 	end
 end);
+
+function isElementAttachedToBone(element)
+	if elm(element) then
+		if Attachemenets[element] then
+			return true;
+		end
+	end
+	return false;
+end
+
+function getElementBoneAttachmentDetails(element)
+	if elm(element) then
+		if Attachemenets[element] then
+			return Attachemenets[element];
+		end
+	end
+	return false;
+end
+
+function setElementBonePositionOffset(element, offsetx, offsety, offsetz)
+	if elm(element) then
+		local elmT = Attachemenets[element] or nil;
+		if elmT then
+			local ped = elmT[1];
+			local bone = elmT[2];
+			local mat = elmT[6];
+			local tableSynch = {ped, bone, offsetx, offsety, offsetz, mat};
+			Attachemenets[element] = tableSynch;
+			triggerClientEvent(root, "sync_pos_attachements", resourceRoot, element, tableSynch);
+			return true;
+		end
+	end
+	return false;
+end
+
+function setElementBoneRotationOffset(element, offsetrx, offsetry, offsetrz)
+	if elm(element) then
+		local elmT = Attachemenets[element] or nil;
+		if elmT then
+			local ped = elmT[1];
+			local bone = elmT[2];
+			local ox = elmT[3];
+			local oy = elmT[4];
+			local oz = elmT[5];
+			local rotMat = calculateMatrix(offsetrx, offsetry, offsetrz);
+			local tableSynch = {ped, bone, ox, oy, oz, rotMat};
+			Attachemenets[element] = tableSynch;
+			triggerClientEvent(root, "sync_pos_attachements", resourceRoot, element, tableSynch);
+			return true;
+		end
+	end
+	return false;
+end
